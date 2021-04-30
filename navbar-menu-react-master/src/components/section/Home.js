d401edc061e6fde185630b5016e441398074c246
Xizafren <56082840+Xizafren@users.noreply.github.com>
2021-04-30 10:43:32 +0200

    Ok now its the last commit.

--- navbar-menu-react-master/src/components/section/Home.js
+++ navbar-menu-react-master/src/components/section/Home.js
@@ -4,11 +4,13 @@ import React, { Component } from 'react'
 export class Home extends Component {
     render() {
         return (
+            
             <div className='container_home'>
-            <h2 className='welcome'>Welcome to Doggy Daycare</h2>
-            <p> Hi, our website is for people who want a dog sitter.<br></br>
-                We work any day but, need a time in between these hours, 
-                <br></br>  weekdays 4:45-7:00 weekends 10:00-6:00. If you want us to come over to your home or dog park,
+
+            <h2>Welcome to Doggy Daycare</h2>
+            <p> Hi, our website is for people who want a dog sitter.
+                <br></br>  We work any day but, need a time in between these hours, 
+                <br></br>  weekdays 4:45-7:00 weekends 10:00-6:00.<br></br> If you want us to come over to your home or dog park,
                 <br></br>  that's fine, we can also take him/her to a huge yard, lots of food and toys.</p>
        </div>
    
